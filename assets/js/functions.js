0e168422005aa79501236cef0d8e0eb2ca9fe575
Katherine <katgm10@gmail.com>
2017-06-23 16:16:10 -0700

    too reliant on px

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
-console.log("🚨 WOOP WOOP 🚨 ITS KATHERINE 👩‍💻 👩‍🎓 WELCOME TO MY LIT 🔥 WEBSITE 💻 IF YOU WANT TO COPY 📄📄 AND PASTE 📋 THIS CODE, GO 🏃‍ TO WWW.GITHUB.COM/KATM10/KATM10.GITHUB.IO ");
-
+  console.log("🚨 WOOP WOOP 🚨 ITS KATHERINE 👩‍💻 👩‍🎓 WELCOME TO MY LIT 🔥 WEBSITE 💻 IF YOU WANT TO COPY 📄📄 AND PASTE 📋 THIS CODE, GO 🏃‍ TO WWW.GITHUB.COM/KATM10/KATM10.GITHUB.IO ");
+  // desk image is 2992 * 2000px
+  function setWindowHeight(){
+    var windowHeight = window.innerHeight;
+    document.body.style.height = windowHeight + "2000px";
+    console.log(document.body.style.height);
+}
+window.addEventListener("resize",setWindowHeight,false);
 });
