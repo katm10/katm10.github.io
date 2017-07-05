3f87657bb5d55fb80ee4024e6b390a2f9159f698
Katherine <katgm10@gmail.com>
2017-07-05 11:08:31 -0700

    back to new

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+window.onload = function (){
+    console.log("🚨 WOOP WOOP 🚨 ITS KATHERINE 👩‍💻 👩‍🎓 WELCOME TO MY LIT 🔥 WEBSITE 💻 IF YOU WANT TO COPY 📄📄 AND PASTE 📋 THIS CODE, GO 🏃‍ TO WWW.GITHUB.COM/KATM10/KATM10.GITHUB.IO ");
+  // desk image is 2992 * 2000px
+  var windowWidth = window.innerWidth;
+  var pix = (windowWidth*2000/2992) + 130;
+  console.log(pix);
+  document.body.style.height = pix + 'px';
+  console.log(document.body.style.height);
+
+}
---
+++ assets/js/latest-hacks.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+    console.log("🚨 WOOP WOOP 🚨 ITS KATHERINE 👩‍💻 👩‍🎓 WELCOME TO MY LIT 🔥 WEBSITE 💻 IF YOU WANT TO COPY 📄📄 AND PASTE 📋 THIS CODE, GO 🏃‍ TO WWW.GITHUB.COM/KATM10/KATM10.GITHUB.IO ");
+  // desk image is 2992 * 2000px
+  var windowWidth = window.innerWidth;
+  var length = (windowWidth*2000/2992);
+  document.getElementById('figure').style.height = length + 'px';
+  document.getElementById('figure').style.width = windowWidth + 'px';
+}
